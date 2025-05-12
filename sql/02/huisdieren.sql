792b26d79e2089244b264ab26d4f06473c7d01fd
Nick Esselman <nick.esselman@gmail.com>
2025-05-12 14:24:03 +0200

    opd 02

---
+++ sql/02/huisdieren.sql
@@ -0,0 +1,34 @@
+INSERT INTO
+  `huisdieren` (
+    `id`,
+    `naam`,
+    `eigenaar`,
+    `typedier`,
+    `leeftijd`,
+    `geboortedatum`
+  )
+VALUES
+  (
+    '0',
+    'otman',
+    'jesper',
+    'hairy dog',
+    '99',
+    '2030-05-15'
+  ),
+  (
+    '1',
+    'furkan',
+    'jesper',
+    'bald dog',
+    '-3',
+    '2030-05-15'
+  ),
+  (
+    '3',
+    'bram',
+    'jesper',
+    'small dog',
+    '1',
+    '2030-05-15'
+  );
