e676aa8e37172353232fe5acdd4b68f48960bd0d
Nick Esselman <nick.esselman@gmail.com>
2025-05-15 12:26:21 +0200

    03/03&04 done

---
+++ sql/03 complex select/limiet.sql
@@ -0,0 +1,3 @@
+SELECT * FROM huisdieren LIMIT 2;
+
+SELECT * FROM huisdieren LIMIT 0, 3;
---
+++ sql/03 complex select/order.sql
@@ -0,0 +1,5 @@
+SELECT * FROM huisdieren ORDER BY naam;
+SELECT * FROM huisdieren ORDER BY naam ASC;
+SELECT * FROM huisdieren ORDER BY naam DESC;
+SELECT * FROM huisdieren ORDER BY typedier, naam;
+SELECT * FROM huisdieren ORDER BY typedier ASC, naam DESC;
