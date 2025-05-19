46fc930b5c11d43e679a0556dc4c3826b1748835
Nick Esselman <nick.esselman@gmail.com>
2025-05-19 14:03:43 +0200

    04/02

--- sql/where.sql
+++ sql/where.sql
@@ -1,3 +1,9 @@
 SELECT * FROM huisdieren WHERE leeftijd <=4;
 SELECT * FROM huisdieren WHERE naam = 'bram';
 SELECT * FROM huisdieren WHERE eigenaar = 'jesper';
+
+
+SELECT * FROM huisdieren WHERE eigenaar = 'joep' AND leeftijd > 1;
+SELECT * FROM huisdieren WHERE (eigenaar = 'joep' OR eigenaar = 'marije') AND leeftijd = 1;
+SELECT * FROM huisdieren WHERE typedier = 'hamster' AND leeftijd = 1;
+SELECT * FROM huisdieren WHERE typedier = 'hamster' AND eigenaar = 'marije' AND leeftijd > 1;
